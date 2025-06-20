7ea33b485631f5c76cb15f68b9f5c47003f8c195
Abisoye Abimbola <haryishar2@gmail.com>
2025-06-20 12:57:04 +0100

    wwtbas updated coin

--- wwtbas/frontend/wwtbas-frontend/src/components/AddQuestion.tsx
+++ wwtbas/frontend/wwtbas-frontend/src/components/AddQuestion.tsx
@@ -4,11 +4,9 @@ import { getFullnodeUrl } from "@mysten/sui/client";
 import { fromBase64 } from "@mysten/sui/utils";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 
-const client = new SuiClient({
-    url: rpcUrl,
-});
+const client = new SuiClient({url: rpcUrl});
 const secretKey = fromBase64('AAGBPmRnaZqKB41nIIn/yG028dsKYuHpCWfqU06nTmKJ');
-const keypai = Ed25519Keypair.fromSecretKey
+const keypair = Ed25519Keypair.fromSecretKey(secretKey);
 
 
 const AddQuestion = () => {
@@ -16,8 +14,10 @@ const AddQuestion = () => {
 
         const tx = new Transaction();
         tx.setSender(keypair.toSuiAddress())
-        tx.setGasBudget(10000000);
-        tx.setGasPayment([{objectId: ''}])
+        tx.setGasBudget(100000000);
+        tx.setGasPayment([{objectId: '0xace29298104e9f8a355d4498e032c2304997f9a048947cee86625b9e402e0cf4 ',
+             version: '447320770',
+              digest: ''}]);
         tx.moveCall ({
             package: '0x67c96623968e700818a49a570d2d3fd58072ab17eb7021e386dc5d2baf515c95',
             module: 'wwtbas',
@@ -27,8 +27,7 @@ const AddQuestion = () => {
         client.signAndExecuteTransaction({signer: Keypair, transaction: tx})
         .catch((e)=>{
             console.log(e);
-
-        })then((res) => {
+        }).then((res) => {
             console.log(res);
         });
     }
